74ae336b8f0b7dc4ed6ecd42ded265e173ed64c3
AKKI-SONI <av6444757@gmail.com>
2024-12-15 23:51:25 +0530

    Initial

--- Backend/models/blacklistToken.model.js
+++ Backend/models/blacklistToken.model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const blacklistTokenSchema = new mongoose.schema({
+const blacklistTokenSchema = new mongoose.Schema({
   token: {
     type: String,
     required: true,
