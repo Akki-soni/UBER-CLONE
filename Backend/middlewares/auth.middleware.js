34463bca3bb34b9c940a36604c6cb629765f1892
AKKI-SONI <av6444757@gmail.com>
2024-12-17 04:01:34 +0530

    Initial

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -9,7 +9,7 @@ module.exports.authUser = async (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  const isBlacklisted = await userModel.fineOne({ token: token });
+  const isBlacklisted = await blacklistTokenModel.fineOne({ token: token });
 
   if (isBlacklisted) {
     return res.status(401).json({ message: "Unauthorized" });
@@ -22,5 +22,28 @@ module.exports.authUser = async (req, res, next) => {
     return next();
   } catch (err) {
     return res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+module.exports.authCaptain = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const isBlacklisted = await blacklistTokenModel.findOne({ token: token });
+
+  if (isBlacklisted) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const captain = await captainModel.findById(decoded._id);
+
+    req.captain = captain;
+  } catch (err) {
+    res.status(401).json({ message: "Unauthorized" });
   }
 };
