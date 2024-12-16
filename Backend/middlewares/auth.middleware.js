33160d466484f531afbf48d425d36c377ab31b19
AKKI-SONI <av6444757@gmail.com>
2024-12-17 04:15:38 +0530

    Initial

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ const userModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const blacklistTokenModel = require("../models/blacklistToken.model");
+const captainModel = require("../models/captain.model");
 
 module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
@@ -43,6 +44,7 @@ module.exports.authCaptain = async (req, res, next) => {
     const captain = await captainModel.findById(decoded._id);
 
     req.captain = captain;
+    return next();
   } catch (err) {
     res.status(401).json({ message: "Unauthorized" });
   }
