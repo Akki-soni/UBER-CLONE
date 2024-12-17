fb6b663f3cdca3f95a65257ea1037198293c7359
AKKI-SONI <av6444757@gmail.com>
2024-12-17 16:33:41 +0530

    Initial

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const userLogin = () => {
-  return <div>userLogin</div>;
+  return (
+    <div>
+      <form>
+        <h3>What's your Email?</h3>
+        <input required type="email" placeholder="email@example.com" />
+      </form>
+    </div>
+  );
 };
 
 export default userLogin;
