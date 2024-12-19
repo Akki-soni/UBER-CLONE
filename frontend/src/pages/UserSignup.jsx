7e2e41684c50b13b98c23769a5e5ec0cc8828c60
AKKI-SONI <av6444757@gmail.com>
2024-12-20 00:28:56 +0530

    Initial

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import UserContext from "../context/UserContext";
+import { UserDataContext } from "../context/UserContext";
 
 const UserSignup = () => {
   const [email, setEmail] = useState("");
@@ -12,31 +12,29 @@ const UserSignup = () => {
 
   const navigate = useNavigate();
 
-  const { user, setUser } = React.useContext(UserContext);
+  const { user, setUser } = useContext(UserDataContext);
 
   const submitHandler = async (e) => {
     e.preventDefault();
     const newUser = {
-      fullName: {
-        firstName: firstName,
-        lastName: lastName,
+      fullname: {
+        firstname: firstName,
+        lastname: lastName,
       },
       email: email,
       password: password,
     };
 
     const response = await axios.post(
-      `${import.meta.env.VITE_BASE_URL}/user/register`,
+      `${import.meta.env.VITE_BASE_URL}/users/register`,
       newUser
     );
     if (response.status === 201) {
       const data = response.data;
-
       setUser(data.user);
-
+      localStorage.setItem("token", data.token);
       navigate("/home");
     }
-
     setEmail("");
     setPassword("");
     setFirstName("");
