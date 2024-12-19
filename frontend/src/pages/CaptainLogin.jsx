eb38c0975449e386fa2af7294ef4e32907627637
AKKI-SONI <av6444757@gmail.com>
2024-12-19 16:14:47 +0530

    Initial

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -1,35 +1,17 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import { CaptainDataContext } from "../context/CapatainContext";
 
-const Captainlogin = () => {
+const CaptainLogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [captainData, setCaptainData] = useState("");
 
-  const { captain, setCaptain } = React.useContext(CaptainDataContext);
-  const navigate = useNavigate();
-
-  const submitHandler = async (e) => {
+  const submitHandler = (e) => {
     e.preventDefault();
-    const captain = {
+    setCaptainData({
       email: email,
       password,
-    };
-
-    const response = await axios.post(
-      `${import.meta.env.VITE_BASE_URL}/captains/login`,
-      captain
-    );
-
-    if (response.status === 200) {
-      const data = response.data;
-
-      setCaptain(data.captain);
-      localStorage.setItem("token", data.token);
-      navigate("/captain-home");
-    }
+    });
 
     setEmail("");
     setPassword("");
@@ -40,45 +22,41 @@ const Captainlogin = () => {
         <img
           className="w-20 mb-3"
           src="https://www.svgrepo.com/show/505031/uber-driver.svg"
-          alt=""
+          alt="Uber-Logo"
         />
-
         <form
           onSubmit={(e) => {
             submitHandler(e);
           }}
         >
-          <h3 className="text-lg font-medium mb-2">What's your email</h3>
+          <h3 className="text-lg font-medium mb-2">What's your Email?</h3>
           <input
             required
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
             }}
-            className="bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base"
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
             type="email"
             placeholder="email@example.com"
           />
-
           <h3 className="text-lg font-medium mb-2">Enter Password</h3>
-
           <input
-            className="bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base"
+            required
             value={password}
             onChange={(e) => {
               setPassword(e.target.value);
             }}
-            required
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
             type="password"
             placeholder="password"
           />
-
-          <button className="bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base">
+          <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base">
             Login
           </button>
         </form>
         <p className="text-center">
-          Join a fleet?{" "}
+          join a fleet?{" "}
           <Link to="/captain-signup" className="text-blue-600">
             Register as a Captain
           </Link>
@@ -87,7 +65,7 @@ const Captainlogin = () => {
       <div>
         <Link
           to="/login"
-          className="bg-[#d5622d] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base"
+          className="bg-[#d5622d] flex items-center justify-center text-white font-semibold mb-5 rounded px-4 py-2 w-full text-lg placeholder:text-base"
         >
           Sign in as User
         </Link>
@@ -96,4 +74,4 @@ const Captainlogin = () => {
   );
 };
 
-export default Captainlogin;
+export default CaptainLogin;
--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,37 +1,17 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { UserDataContext } from "../context/UserContext";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
-const UserLogin = () => {
+const userLogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [userData, setUserData] = useState({});
 
-  const { user, setUser } = useContext(UserDataContext);
-  const navigate = useNavigate();
-
-  const submitHandler = async (e) => {
+  const submitHandler = (e) => {
     e.preventDefault();
-
-    const userData = {
+    setUserData({
       email: email,
       password: password,
-    };
-
-    const response = await axios.post(
-      `${import.meta.env.VITE_BASE_URL}/users/login`,
-      userData
-    );
-
-    if (response.status === 200) {
-      const data = response.data;
-      setUser(data.user);
-      localStorage.setItem("token", data.token);
-      navigate("/home");
-    }
-
+    });
     setEmail("");
     setPassword("");
   };
@@ -41,41 +21,37 @@ const UserLogin = () => {
       <div>
         <img
           className="w-16 mb-10"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQy-OIkA6In0fTvVwZADPmFFibjmszu2A0g&s"
-          alt=""
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt="Uber-Logo"
         />
-
         <form
           onSubmit={(e) => {
             submitHandler(e);
           }}
         >
-          <h3 className="text-lg font-medium mb-2">What's your email</h3>
+          <h3 className="text-lg font-medium mb-2">What's your Email?</h3>
           <input
             required
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
             }}
-            className="bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base"
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
             type="email"
             placeholder="email@example.com"
           />
-
           <h3 className="text-lg font-medium mb-2">Enter Password</h3>
-
           <input
-            className="bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base"
+            required
             value={password}
             onChange={(e) => {
               setPassword(e.target.value);
             }}
-            required
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
             type="password"
             placeholder="password"
           />
-
-          <button className="bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base">
+          <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base">
             Login
           </button>
         </form>
@@ -89,7 +65,7 @@ const UserLogin = () => {
       <div>
         <Link
           to="/captain-login"
-          className="bg-[#10b461] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base"
+          className="bg-[#10b461] flex items-center justify-center text-white font-semibold mb-5 rounded px-4 py-2 w-full text-lg placeholder:text-base"
         >
           Sign in as Captain
         </Link>
@@ -98,4 +74,4 @@ const UserLogin = () => {
   );
 };
 
-export default UserLogin;
+export default userLogin;
